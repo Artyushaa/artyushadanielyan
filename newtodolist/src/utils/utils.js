07bd789c73ef95aee032437ed47d76d80a9acaa1
Artyusha <artyushadanielyan@gmail.com>
2023-03-20 16:33:33 +0400

    delete todolist

--- newtodolist/src/utils/utils.js
+++
@@ -1,6 +0,0 @@
-export const getDate = () => {
-    const fullDate = new Date();
-    const date = fullDate.getDate() + "." + fullDate.getMonth() + 1 + '.' + fullDate.getFullYear();
-    return date
-}
-
